5b82453d65bfceb2ce5c67675f1b66a3cc431ebe
Adam Bull <adamjamesbull@googlemail.com>
2017-09-03 20:31:14 +0100

    Added basic api client js implementation

---
+++ client/shared/javascripts/api-client.js
@@ -0,0 +1,31 @@
+import request from 'reqwest';
+import { baseApiUri } from 'constants/config';
+
+class ApiClient {
+  constructor(baseUri) {
+    this.baseUri = baseUri;
+    this.requestUrl = '';
+  }
+
+  get(path, params = {}) {
+    return request.get(this.requestUrl(path), params);
+  }
+
+  post(path, body = {}) {
+    return request.post(this.requestUrl(path), body);
+  }
+
+  delete(path, params = {}) {
+    return request.delete(this.requestUrl(path), params);
+  }
+
+  patch(path, body = {}) {
+    return request.patch(this.requestUrl(path), body);
+  }
+
+  requestUrl(path) {
+    return `${this.baseUri}/${path}`;
+  }
+}
+
+export default new ApiClient(baseApiUri);
